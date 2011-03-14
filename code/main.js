34532842963e16f9ebd9bf6494fc64a373af3480
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-14 11:05:52 +0100

    refactoring

--- code/main.js
+++ code/main.js
@@ -1,6 +1,5 @@
 define(function(require, exports, module) {
-jsonml = require("jsonml");
-console.log(jsonml);
+mui = require("mui");
 
 function genWord() {
     var i, length, s;
@@ -22,47 +21,16 @@ function nextText() {
     return s;
 }
 
-function height(dom) {
-    return document.defaultView.getComputedStyle(dom, "").getPropertyValue("height");
-}
-
-function gId(id) {
-    return document.getElementById(id);
-}
-
-function slidein() {
-    window.scroll(0,0);
-
-    var prev = gId("prev");
-    prev.parentNode.removeChild(prev);
-
-    var current = gId("current");
-    current.setAttribute("id", "prev");
-    //current.style.top = "-" + height(next);
-
-    var next = gId("next");
-    next.setAttribute("id", "current");
-
-    gId("container").style.height = height(next);
-
-    //prev.style.display = "none"
-}
-
 handleClick = function() {
-    next = document.createElement("div");
-    next.setAttribute("id", "next");
-    next.setAttribute("onClick", "handleClick()");
-    next.innerHTML= [ 
-            ["div", {"class": "topbar"}, "Sp\xf8rgetjenesten"], 
+    mui.showPage([
+            ["div", {"class": "header"}, "Sp\xf8rgetjenesten"], 
             ["div", {"class": "contentbox"}, nextText()],
             ["div", {"class": "contentbox"}, nextText()],
             ["div", {"class": "contentbox"}, nextText()],
-            ["div", {"class": "topbar"}, "Hello", "world"]
-        ].map(jsonml.toXml).join('');
-    var current = gId("current");
-    gId("container").insertBefore(next, current);
-    current.style.top = "-" + height(next);
-    setTimeout(slidein, 0);
+            ]);
 }
+require.ready(function() {
+    handleClick();
+});
 
 });
--- update_externals.sh
+++ update_externals.sh
@@ -3,3 +3,5 @@ curl http://requirejs.org/docs/release/0.23.0/minified/require.js > require.js
 echo 'define(function(require, exports, module) {' > jsonml.js
 curl https://github.com/rasmuserik/jsonml/raw/master/jsonml.js >> jsonml.js
 echo '});' >> jsonml.js
+curl https://github.com/rasmuserik/mui/raw/master/html5/mui.js >> mui.js
+curl https://github.com/rasmuserik/mui/raw/master/html5/mui.css >> mui.css
