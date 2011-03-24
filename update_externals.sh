0ae2376ce9c47c943cb36a673ecb150cbb72a22f
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-03-24 14:21:20 +0100

    update-externals-removed

--- update_externals.sh
+++
@@ -1,6 +0,0 @@
-cd code
-mkdir mui
-cd mui
-curl https://github.com/rasmuserik/jsonml/raw/master/jsonml.js > jsonml.js
-curl https://github.com/rasmuserik/mui/raw/master/html5/mui.js > mui.js
-curl https://github.com/rasmuserik/mui/raw/master/html5/mui.css > mui.css
